4ba46757909bf7a830acdb13cae9b8a68b52c7c7
Wu SongYu <wuwilliam2002@gmail.com>
2024-11-19 19:59:06 +0000

    added routing for posting a new task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import { tasksData } from './tasksData';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router(); 
+
+router.get('/', (req, res) => {
+    res.json(tasksData);
+});
+
 
 router.get('/:id', (req, res) => {
     const { id } = req.params
@@ -10,6 +16,21 @@ router.get('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
     return res.status(200).json(task);
+});
+
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
 });
 
 export default router;
